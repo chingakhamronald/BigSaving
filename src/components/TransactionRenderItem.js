18770e7ac274fc4fb0133cb607675dbc199c7644
chingakhamronald <ronaldoch.lamzing@gmail.com>
2023-04-07 21:56:45 +0530

    some ui change for TransactionRenderItem screen

--- src/components/TransactionRenderItem.js
+++ src/components/TransactionRenderItem.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import {TouchableOpacity} from 'react-native';
+import {StyleSheet, TouchableOpacity, View} from 'react-native';
 import {Text} from 'react-native-paper';
 
 const TransactionRenderItem = ({item}) => {
   return (
-    <TouchableOpacity>
-      <Text>{item.name}</Text>
-      <Text> {item.payment_type}</Text>
-      <Text>Rs {item.amount}</Text>
+    <TouchableOpacity style={styles.wrapper}>
+      <View style={styles.flex3}>
+        <Text>{item.name}</Text>
+        <Text>Payment Type: {item.payment_type}</Text>
+      </View>
+      <View style={styles.flex1}>
+        <Text>Rs {item.amount}</Text>
+      </View>
     </TouchableOpacity>
   );
 };
 
 export default TransactionRenderItem;
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flexDirection: 'row',
+    padding: 5,
+  },
+  flex1: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+  flex3: {flex: 3},
+});
